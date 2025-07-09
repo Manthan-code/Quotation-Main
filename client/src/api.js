8b149eed6f2b5d2525dcfdaf8191bec089f28a02
Manthan Patel <manthanpatel150306@gmail.com>
2025-07-09 12:42:51 +0530

    Sync local changes before pull

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://quotation-api.onrender.com/api',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 api.interceptors.request.use((config) => {
