95c1972ebb1ba2e661debbb54abd8055a769c5cb
Manthan Patel <manthanpatel150306@gmail.com>
2025-07-09 15:31:08 +0530

    Cleanup: removed duplicated server files from root

--- models/Product.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const productSchema = new Schema({
-  series:    { type: String, required: true },
-  typology:  { type: String, required: true },
-  type:      { type: String }
-}, { timestamps: false });
-
-module.exports = model('Product', productSchema);
--- models/Unit.js
+++
@@ -1,9 +0,0 @@
-// models/Unit.js
-const { Schema, model } = require('mongoose');
-module.exports = model('Unit', new Schema(
-  {
-    unit:       { type: String, required: true },
-    shortForm:  { type: String },
-  },
-  { timestamps: false }
-));
